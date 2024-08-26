43fcc3e95bd0c1fb3d34d807381e818034b66632
Jahid01234 <mdjahidhasan790523@gmail.com>
2024-08-26 11:58:48 +0600

    - add search screen

--- lib/ui/bottom_nav_screen/home_screen.dart
+++ lib/ui/bottom_nav_screen/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:e_commerce_app_with_firebase/constant/Colors/app_colors.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:e_commerce_app_with_firebase/ui/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -19,12 +19,10 @@ class _HomeScreenState extends State<HomeScreen> {
   final List productsList = [];
   int _dotsPosition = 0;
 
-
   // Get CarouselImages data from firebase
   Future<void> _getCarouselImages() async {
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-    QuerySnapshot qn =
-        await firebaseFirestore.collection("carousel-slider").get();
+    QuerySnapshot qn = await firebaseFirestore.collection("carousel-slider").get();
 
     for (int i = 0; i < qn.docs.length; i++) {
       setState(() {
@@ -35,23 +33,19 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-
   // Get Product data from firebase
   Future<void> _getProductDetails() async {
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-    QuerySnapshot qn =
-    await firebaseFirestore.collection("products").get();
+    QuerySnapshot qn = await firebaseFirestore.collection("products").get();
 
     for (int i = 0; i < qn.docs.length; i++) {
       setState(() {
-        productsList.add(
-          {
-            "product-name" : qn.docs[i]["product-name"],
-            "product-description" : qn.docs[i]["product-description"],
-            "product-img" : qn.docs[i]["product-img"],
-            "product-price" : qn.docs[i]["product-price"]
-          }
-        );
+        productsList.add({
+          "product-name": qn.docs[i]["product-name"],
+          "product-description": qn.docs[i]["product-description"],
+          "product-img": qn.docs[i]["product-img"],
+          "product-price": qn.docs[i]["product-price"]
+        });
       });
     }
   }
@@ -66,136 +60,189 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("E-Commerce"),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              //crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // search box
-                Row(
-                  children: [
-                    Expanded(
-                      child: SizedBox(
-                        height: 60.h,
-                        child: TextFormField(
-                          controller: _searchTEController,
-                          decoration: const InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Search by product',
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(0),
-                                ),
-                                borderSide: BorderSide(color: Colors.cyan)),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(0),
-                                ),
-                                borderSide: BorderSide(color: Colors.orange)),
+      resizeToAvoidBottomInset: false ,
+      appBar: AppBar(
+        title: const Text("E-Commerce"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // search box
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 60.h,
+                      child: TextFormField(
+                        controller: _searchTEController,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search by product',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(0),
+                              ),
+                              borderSide: BorderSide(
+                                  color: Colors.cyan,
+                              ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(0),
+                              ),
+                              borderSide: BorderSide(color: Colors.orange),
                           ),
                         ),
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>const SearchScreen()));
+                        },
                       ),
                     ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: 60.h,
-                        width: 60.w,
-                        decoration: BoxDecoration(
-                            color: AppColors.deep_orange,
-                            borderRadius: BorderRadius.circular(0)),
-                        child: const Icon(
-                          Icons.search,
-                          color: AppColors.white,
-                        ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 60.h,
+                      width: 60.w,
+                      decoration: BoxDecoration(
+                          color: AppColors.deep_orange,
+                          borderRadius: BorderRadius.circular(0),
+                      ),
+                      child: const Icon(
+                        Icons.search,
+                        color: AppColors.white,
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: 20.h),
+                  ),
+                ],
+              ),
 
-                // show image
-                AspectRatio(
-                  aspectRatio: 3.5,
-                  child: CarouselSlider(
-                      items: carouselImagesList
-                          .map(
-                            (item) => ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image: NetworkImage(item),
-                                      fit: BoxFit.fitWidth,
-                                  ),
-                                ),
+
+              // show image
+              SizedBox(height: 20.h),
+              AspectRatio(
+                aspectRatio: 3.5,
+                child: CarouselSlider(
+                  items: carouselImagesList
+                      .map(
+                        (item) => ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(item),
+                                fit: BoxFit.fill,
                               ),
                             ),
-                          ).toList(),
-                      options: CarouselOptions(
-                        viewportFraction: 0.8,
-                        initialPage: 0,
-                        enableInfiniteScroll: true,
-                        reverse: false,
-                        autoPlay: true,
-                        autoPlayInterval: const Duration(seconds: 3),
-                        autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                        autoPlayCurve: Curves.fastOutSlowIn,
-                        enlargeCenterPage: true,
-                        enlargeFactor: 0.3,
-                        onPageChanged: (value,carouselPageChangedReason){
-                          setState(() {
-                            _dotsPosition = value;
-                          });
-                        },
-                        scrollDirection: Axis.horizontal,
-                      ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  options: CarouselOptions(
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 3),
+                    autoPlayAnimationDuration:
+                        const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    onPageChanged: (value, carouselPageChangedReason) {
+                      setState(() {
+                        _dotsPosition = value;
+                      });
+                    },
+                    scrollDirection: Axis.horizontal,
                   ),
                 ),
-                SizedBox(height: 10.h),
+              ),
 
-                // Dots Indicator show
-                DotsIndicator(
-                    dotsCount: carouselImagesList.isEmpty ? 1:carouselImagesList.length,
-                    position: _dotsPosition,
-                    decorator: const DotsDecorator(
-                      activeColor: AppColors.deep_orange,
-                    ),
+
+              // Dots Indicator show
+              SizedBox(height: 10.h),
+              DotsIndicator(
+                dotsCount: carouselImagesList.isEmpty ? 1 : carouselImagesList.length,
+                position: _dotsPosition,
+                decorator: const DotsDecorator(
+                  activeColor: AppColors.deep_orange,
                 ),
-                SizedBox(height: 15.h),
+              ),
 
-                // show product details
-                Expanded(
-                    child: GridView.builder(
-                      scrollDirection: Axis.horizontal,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 1
+              // Text Part(top Products,view all)
+              SizedBox(height: 15.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Top Products",
+                    style:  TextStyle(
+                      color: AppColors.deep_orange,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "View All",
+                        style: TextStyle(
+                          color: AppColors.deep_orange,
                         ),
-                        itemBuilder: (_,index){
-                          return Card(
-                            elevation: 1,
-                            child: Column(
-                              children: [
-                                AspectRatio(aspectRatio: 2,child: Image.network(productsList[index]["product-img"][0])),
-                                Text("${ productsList[index]["product-name"]}"),
-                                Text("${ productsList[index]["product-price"]}"),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: const Icon(Icons.arrow_forward_ios,
+                            size: 15, color: AppColors.deep_orange,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
 
 
-                              ],
+              // show product details
+              SizedBox(height: 15.h),
+              Expanded(
+                child: GridView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: productsList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: 1),
+                  itemBuilder: (_, index) {
+                    return Card(
+                      elevation: 1,
+                      child: Column(
+                        children: [
+                          AspectRatio(
+                              aspectRatio: 2,
+                              child: Image.network(
+                                  productsList[index]["product-img"][0],
+                                  fit: BoxFit.fill)),
+                          SizedBox(height: 10.h),
+                          Text(
+                            "${productsList[index]["product-name"]}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
                             ),
-                          );
-                        },
-                    ),
+                          ),
+                          Text("${productsList[index]["product-price"]}"),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
---
+++ lib/ui/search_screen.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  //final TextEditingController _searchTEController = TextEditingController();
+  late String inputText= '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Search Page"),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                //controller: _searchTEController,
+                onChanged: (value){
+                  inputText=value;
+                  setState(() {});
+                  },
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Search by product',
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(0),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.cyan,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(0),
+                    ),
+                    borderSide: BorderSide(color: Colors.orange),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15.h),
+              const Text("Search Product List",style: TextStyle(fontWeight: FontWeight.bold),),
+              SizedBox(height: 8.h),
+              Expanded(
+                  child: Container(
+                    child: StreamBuilder(
+                      stream: FirebaseFirestore.instance.
+                      collection("products").where("product-name",isEqualTo: inputText).snapshots(),
+                      builder: (context,snapshot){
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        } else if (snapshot.hasError) {
+                          return Center(child: Text('Error: ${snapshot.error}'));
+                        } else if (!snapshot.hasData ) {
+                          return const Center(child: Text('No photos found'));
+                        } else {
+                           return ListView(
+                             children: snapshot.data!.docs.map((DocumentSnapshot document){
+                                 Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                                 return Card(
+                                   child: ListTile(
+                                     title: Text(data["product-name"]),
+                                     leading: Image.network(data["product-img"][0]),
+                                   ),
+                                 );
+                             }).toList(),
+                           );
+                          }
+                      },
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
